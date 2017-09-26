e31b8bd588b68f240be467a15197e7593ec476ca
Deno <denis.akopyan@seznam.cz>
2017-09-26 14:20:09 +0200

    Change the way methods return values

--- BackPropNeuralNetworkCPP/Layer.h
+++ BackPropNeuralNetworkCPP/Layer.h
@@ -19,12 +19,12 @@ public:
 #pragma endregion
 
 #pragma region Constructor
-  
+
   /**
-   * \brief Default constructor
-   * \param numberOfInputs 
-   * \param numberOfOutputs 
-   */
+  * \brief Default constructor
+  * \param numberOfInputs
+  * \param numberOfOutputs
+  */
   Layer(int numberOfInputs, int numberOfOutputs);
 
 #pragma endregion
@@ -32,13 +32,13 @@ public:
 #pragma region Methods
 
   /**
-   * \brief Fills weights with random values
-   */
+  * \brief Fills weights with random values
+  */
   void InitilizeWeights() const;
-  
+
   /**
-   * \brief Fill weights with new values
-   */
+  * \brief Fill weights with new values
+  */
   void UpdateWeights() const;
 
   /**
@@ -49,30 +49,30 @@ public:
   static double Layer::ActivateFunction(double value);
 
   /**
-   * \brief Calculates the derivative of given value
-   * \param value Value of which the derivative is to be calculated
-   * \return Calculation
-   */
+  * \brief Calculates the derivative of given value
+  * \param value Value of which the derivative is to be calculated
+  * \return Calculation
+  */
   static double DeriveFunction(double value);
-  
+
   /**
-   * \brief Feeds value to next layer
-   * \param inputs Input data
-   * \return Neural network prediction
-   */
-  double* FeedForward(double* inputs);
-  
+  * \brief Feeds value to next layer
+  * \param inputs Input data
+  * \return Neural network prediction
+  */
+  void FeedForward(double* inputs, double** retVal = nullptr);
+
   /**
-   * \brief Back propagation algorythm from output to penultimate layer
-   * \param expected Expected result
-   */
+  * \brief Back propagation algorythm from output to penultimate layer
+  * \param expected Expected result
+  */
   void BackPropOutput(double* expected) const;
 
   /**
-   * \brief Back propagation algorythm from penultimate to input layer
-   * \param gammaForward 
-   * \param weightsForward 
-   */
+  * \brief Back propagation algorythm from penultimate to input layer
+  * \param gammaForward
+  * \param weightsForward
+  */
   void BackPropHidden(double* gammaForward, double* weightsForward) const;
 
 #pragma endregion
@@ -88,8 +88,8 @@ private:
 #pragma region Constants
 
   /**
-   * \brief Neural network learning rate
-   */
+  * \brief Neural network learning rate
+  */
   const double learningRate = 0.02;
 
 #pragma endregion
