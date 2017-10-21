34c4730be31af425c6088baf14148b38fbe50e29
Deno <denis.akopyan@seznam.cz>
2017-10-21 13:16:03 +0400

    Multiple changes  - Upgrade to VS 2017  - Add documentation  - Create new project Data  - Add class Generator to Data

--- BackPropNeuralNetworkCS/Program.cs
+++ BackPropNeuralNetworkCS/Program.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
-using System.Dynamic;
 
 namespace NeuralNetworkFacadeCS
 {
   internal static class Program
   {
+    // TODO Automate Training data generation
     private static readonly double[,] TrainingData = {
       { 0, 0, 0, 0 },
       { 0, 0, 1, 0 },
@@ -25,6 +26,7 @@ namespace NeuralNetworkFacadeCS
       { 1, 1, 1, 1 },
     };
 
+    // TODO Automate Expected data generation
     private static readonly double[,] ExpectedData = {
       { 0, 0 },
       { 0, 0 },
@@ -48,20 +50,23 @@ namespace NeuralNetworkFacadeCS
     {
       while (true)
       {
-        var layers = GetLayers();
-        var iterations = GetInputInt("N times to test: ");
-        var stable = true;
-        var totalError = 0.0;
+        var layers = GetLayers();                             // Getting hidden layer definition
+        var iterations = GetInputInt("N times to test: ");    // Getting test iterations count
+        var stable = true;                                    // Test success
+        var totalError = 0.0;                                 // An average error of all tests combined
 
+        // Testing network
         for (var i = 0; i < iterations; i++)
         {
+          // Initializing new network
           var net = new FacadeX64(layers, TrainingData, ExpectedData);
-
+          // Training network
           net.TrainNetwork();
-
+          // Evaluating gathered information
           net.OutputToConsoleTest(ref totalError, ref stable, true);
         }
 
+        // Test summary
         Console.WriteLine($"Stability: {(stable ? "Stable" : "Unstable")}\nError: {totalError / iterations}");
 
         Console.ReadKey();
@@ -69,9 +74,13 @@ namespace NeuralNetworkFacadeCS
       }
     }
 
+    /// <summary>
+    /// Request user to provide layer definition
+    /// </summary>
+    /// <returns>An array of N ints which define each layer size, where N is a layer</returns>
     private static int[] GetLayers()
     {
-      var layers = new List<int> {TrainingData.GetLength(1)};
+      var layers = new List<int> { TrainingData.GetLength(1) };
       var hiddenLayers = GetInputInt("Hidden layers count: ");
 
       for (var layer = 0; layer < hiddenLayers; layer++)
@@ -82,17 +91,22 @@ namespace NeuralNetworkFacadeCS
       return layers.ToArray();
     }
 
+    /// <summary>
+    /// Requests integer input from user
+    /// </summary>
+    /// <param name="message">Message displayed to user</param>
+    /// <returns>Requested value</returns>
     private static int GetInputInt(string message)
     {
-      string input;
+      string input;     // Input placeholder
       int output;
       do
-	    {
-	      Console.Write(message);
-	      input = Console.ReadLine();
-	    } while (!int.TryParse(input, out output));
+      {
+        Console.Write(message);
+        input = Console.ReadLine();
+      } while (!int.TryParse(input, out output));
 
-      return output;
+      return output;    // Returning converted value
     }
   }
 }
---
+++ Data/Generator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+  /// <summary>
+  /// Generates a dataset
+  /// </summary>
+  public class Generator
+  {
+    public enum Operation
+    {
+      Conjunction = 1,
+      Disjunction = 2,
+      Implication = 3,
+      Equivelance = 4
+    }
+
+    /// <summary>
+    /// Training data property
+    /// </summary>
+    public double[,] TrainingData { get; set; }
+
+    /// <summary>
+    /// Expected data property
+    /// </summary>
+    public double[,] ExpectedData { get; set; }
+
+    #region Constructor
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <param name="inputCount">Number of elements in <see cref="TrainingData"/></param>
+    /// <param name="outputCount">Number of elements in <see cref="ExpectedData"/></param>
+    /// <param name="operationType">Logical operation which is to be applied to <see cref="TrainingData"/> values to create <see cref="ExpectedData"/></param>
+    public Generator(int inputCount, int outputCount, Operation operationType)
+    {
+      if (inputCount <= outputCount)
+        throw new ArgumentException("Input count less or equals than output count is prohibited.");
+
+      TrainingData = GenerateTrainingData(ref inputCount);
+      ExpectedData = GenerateExpectedData(ref outputCount, ref operationType);
+    }
+
+    #endregion
+
+    #region Methods
+
+    private double[,] GenerateTrainingData(ref int inputCount)
+    {
+      TrainingData = new double[(int)Math.Pow(2, inputCount), inputCount];
+      for (int i = 0; i < TrainingData.GetLength(1); i++)
+      {
+        int incr = (int)Math.Pow(2, i);
+        int bin = 0;
+        bool bit = false;
+
+        for (int j = 0; j < TrainingData.GetLength(0); j++)
+        {
+          if (bin + 1 == incr)
+          {
+            bit = !bit;
+            bin = 0;
+          }
+          else bin++;
+
+          TrainingData[j, i] = bit ? 1 : 0;          
+        }
+      }
+      return null;
+    }
+
+    private double[,] GenerateExpectedData(ref int outputCount, ref Operation operationType)
+    {
+      return null;
+    }
+
+    #endregion
+  }
+}
